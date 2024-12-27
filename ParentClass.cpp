5954cbb607db1e13731a84618b1d0cded711e851
SudarshanDevadiga <sudarshandevadiga200@gmail.com>
2024-12-28 02:25:02 +0530

    Add files via upload

---
+++ ParentClass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+    public:
+        Vehicle() {
+            cout << "This is Vehicle" << endl ;
+        }
+};
+class Car : public Vehicle {
+
+};
+int main(){
+    Car obj;
+    return 0;
+}
